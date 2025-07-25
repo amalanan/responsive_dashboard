6dbe2f6d0f3c8dbe22ab38770873271fd6323df0
Amal Anan <133678416+amalanan@users.noreply.github.com>
2025-07-25 21:46:26 +0300

    Create dashboard_view.dart

---
+++ lib/views/dashboard_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/widgets.dart';
+
+class DashBoardView extends StatelessWidget {
+  const DashBoardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLayout: (context) => SizedBox(),
+        tabletLayout: (context) => SizedBox(),
+        desktopLayout: (context)=> DashboardDesktopLayout(),
+      ),
+    );
+  }
+}
